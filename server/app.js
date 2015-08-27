9416e0c8cb95fdf814f0e970921355d10a947778
James Logan <jamesblogan20@gmail.com>
2015-08-27 14:32:18 -0500

    added socketio and opened a websocket

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var express = require('express');
+var socketio = require('socket.io')
 
 var app = express();
 
-app.listen(3000, function () {
+app.use(express.static('client'));
+
+var server = app.listen(3000, function () {
   console.log('Server listeninig on port 3000')
 });
+
+socketio(server);
